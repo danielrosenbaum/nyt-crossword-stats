d7df4bc633f33c77445d8293a8571582b85bbd89
Daniel Rosenbaum <daniel.rosenbaum10@gmail.com>
2024-03-03 10:55:20 +0200

    Traverse through all puzzles

--- nyt-progress.py
+++ nyt-progress.py
@@ -1,36 +1,22 @@
-
-# python script to create strings of Puzzle IDs to query NYTimes Crossword 
-# Then read the result to find which puzzles have been started but not completed
-
-import json
 import requests
-
-# nyt api query + add on all IDs (max 1000 per request)
-accountID = input("Enter your NYT Games Account ID:")
-query = "https://www.nytimes.com/svc/crosswords/v3/{}/progress.json?puzzle_ids=20788".format(accountID)
-
-for id in range(11000, 11050):
-    query += ',' + str(id)
-
-r = requests.get(url = query)
-
-data = r.json()
-
-print (data)
+import pandas as pd
+from keys import ACCOUNT_ID    
 
 
+start_date = '1994-11-21'
+end_date = '2023-12-31'
 
-# you must manually copy/paste result from api to a file and then read said file making sure it is just a list of objects 
-with open('file.json', 'r') as f:
-    data = json.loads(f.read())
+daterange = pd.date_range(start=start_date, end=end_date, freq='100D')
 
-i = 0 # number of puzzles found to complete 
-# get every started but not completed puzzle and print it out
-for crossword in data:
-    if crossword['publish_type'] == 'Daily' and crossword['solved'] == False and crossword['star'] != 'Gold':
-        print(crossword)
-        i += 1        
-print(i)
+for single_date in daterange:
+    current_date = single_date.strftime("%Y-%m-%d")
+    query = f'https://www.nytimes.com/svc/crosswords/v3/{ACCOUNT_ID}/puzzles.json?publish_type=daily&date_start={start_date}&date_end={end_date}'
+    r = requests.get(url = query)
+    data = r.json()
+    start_date = current_date
+
+
+
 
 
 
